a6b17e4d8cefd5b47859ceb423b586e8db6e34f1
EegArlert <bsyahputra@oakland.edu>
2024-10-18 11:16:57 -0400

    finishing up tic tac toe game and fixing logic horizontal checker

--- tictactoe.js
+++ tictactoe.js
@@ -1,6 +1,7 @@
 /* Creating Board via Factory Function Method*/
 function BoardFactory() {
-    let array = [];
+    console.log("Board Factory Activated")
+    var array = [];
     const createBoardGame = () => {
         for (let i = 0; i <= 2; i++) {
             array[i] = [];
@@ -15,21 +16,24 @@ function BoardFactory() {
     };
 };
 
+
 function Player() {
+    const basePlayer = "X"
     let currentPlayer = "X";
 
     const switchPlayer = () => {
-       currentPlayer === "X" ? currentPlayer = "O" : currentPlayer = "X";
+        currentPlayer === "X" ? currentPlayer = "O" : currentPlayer = "X";
     }
     return {
         switchPlayer,
-        getCurrentPlayer: () => currentPlayer
+        getCurrentPlayer: () => currentPlayer,
+        getBasePlayer: () => basePlayer
     }
 }
 
 
 /* Creating Game Logic */
-function GameLogic() {
+function checkWinning() {
 
     //check row
     const horizontalWinningCheck = (boardGame, current) => {
@@ -43,11 +47,10 @@ function GameLogic() {
     }
     //check column
     const verticalWinningCheck = (boardGame, current) => {
-        const boardCell = document.querySelectorAll(".board-cell");
         for (let i = 0; i < 3; i++) {
-            if (boardGame[i][1] === current &&
-                boardGame[i][2] === current &&
-                boardGame[i][3] === current) {
+            if (boardGame[i][0] === current &&
+                boardGame[i][1] === current &&
+                boardGame[i][2] === current) {
                 return true;
             }
         }
@@ -77,7 +80,7 @@ function GameLogic() {
 
 // verifying
 function AcceptUserInput() {
-    const writeToBoard = (cell, boardGame, player) => {
+    const writeToArray = (cell, boardGame, player) => {
         if (cell.getAttribute('id') == "cell-1") {
             boardGame.getBoard()[0][0] = player;
         } else if (cell.getAttribute('id') == "cell-2") {
@@ -99,18 +102,53 @@ function AcceptUserInput() {
         }
     }
     return {
-        writeToBoard
+        writeToArray
+    }
+}
+
+function displayResult() {
+    const resultContainer = document.querySelector('#result-container');
+    
+    const setDisplay = (winner) => {
+        resultContainer.innerText = "Player " + winner + " win";
+    }
+
+    const resetDisplay = () => {
+        resultContainer.innerText = "";
+    }
+    return {
+        setDisplay,
+        resetDisplay
+    }
+}
+
+function gameControl() {
+    const start = document.getElementById("play-btn");
+
+    const startButtonAction = () => {
+        start.addEventListener("click", () => {
+            console.log("Start button clicked");
+            TicTacToe();
+            start.disabled = true;
+        })
+    }
+
+    return {
+        startButtonAction,
     }
 }
 
 // The Game
 function TicTacToe() {
-    const player = Player();
-    const mark = AcceptUserInput();
-    const boardGame = BoardFactory();
+    console.log("Start Tic tac toe")
+    let player = Player();
+    let mark = AcceptUserInput();
+    let boardGame = BoardFactory();
     boardGame.createBoardGame();
-    const gameLogic = GameLogic();
+    console.log("Board Game Created In TIC TAC TOE")
+    let checkWinner = checkWinning();
     let moveCount = 1;
+    let displayOutcome = displayResult();
 
     /* --------------------------------USER INTERACTION AND DATA DISPLAY------------------------------------------------- */
     const cellBoxes = document.querySelectorAll(".board-cell");
@@ -120,25 +158,59 @@ function TicTacToe() {
 
             cell.innerText = player.getCurrentPlayer();
 
-            mark.writeToBoard(cell, boardGame, player.getCurrentPlayer())
+            mark.writeToArray(cell, boardGame, player.getCurrentPlayer())
 
-            if (gameLogic.horizontalWinningCheck(boardGame.getBoard(), player.getCurrentPlayer()) ||
-                gameLogic.verticalWinningCheck(boardGame.getBoard(), player.getCurrentPlayer()) ||
-                gameLogic.horizontalWinningCheck(boardGame.getBoard(), player.getCurrentPlayer())) {
-                return console.log(`The winner is ${player.getCurrentPlayer()}`);
+            if (checkWinner.horizontalWinningCheck(boardGame.getBoard(), player.getCurrentPlayer()) ||
+                checkWinner.verticalWinningCheck(boardGame.getBoard(), player.getCurrentPlayer()) ||
+                checkWinner.diagonalWinningCheck(boardGame.getBoard(), player.getCurrentPlayer())) {
+
+                cellBoxes.forEach(cell => {
+                    cell.style.backgroundColor = "rgba(240, 248, 255, 0.8)";
+                    cell.style.pointerEvents = "none";
+                })
+
+                return displayOutcome.setDisplay(player.getCurrentPlayer());
+                
             }
 
             moveCount++;
 
-            if(moveCount >= 9) {
+            if (moveCount >= 9) {
                 return "draw";
             }
 
             player.switchPlayer();
+
+
         })
     });
+
+    const reset = document.getElementById("reset-game");
+
+    reset.addEventListener("click", () => {
+        console.log("Reset button clicked");
+        boardGame = BoardFactory();
+        boardGame.createBoardGame();
+        console.log("new board game created");
+        moveCount = 1;
+        player = Player();
+        cellBoxes.forEach(cell => {
+            cell.style.backgroundColor = "rgb(240, 248, 255)";
+            cell.style.pointerEvents = "auto";
+        })
+
+
+
+        cellBoxes.forEach(cell => {
+            cell.innerText = "";
+        })
+
+        displayOutcome.resetDisplay();
+    })
+
+
+
 }
 
-function gameControl() {
-    const start = document.querySelector(".")
-}
+gameControl().startButtonAction();
+// gameControl().resetButtonAction();
